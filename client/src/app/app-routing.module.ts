de52145bb4503dbe1d9b8a8798a908c96824fb40
Hardy Lutula <hardylutula@gmail.com>
2023-11-10 11:19:35 +0200

    project structure + routers + pages added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PrivateComponent } from './layout/private/private.component';
+import { ShortlistPageComponent } from './pages/shortlist-page/shortlist-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '404', component: NotfoundPageComponent },
+  {
+    path: 'private',
+    component: PrivateComponent,
+    children: [
+      { path: 'home', component: HomePageComponent },
+      { path: 'shortlist', component: ShortlistPageComponent },
+      { path: 'admin', component: AdminPageComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
+  { path: '', redirectTo: 'private/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
