857c3ef3ed95380adee078eb41222afeb6cc1439
Samkelisiwe Mkhize <samkelsiwemkhize@gmail.com>
2023-11-14 10:32:59 +0200

    layout changed

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const connectDB = require('./connectDB')
 
 const app = express()
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
